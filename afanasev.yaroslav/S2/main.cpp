f08039c81a1d23af9fa5c52f8c43a74826bc7f1a
jar <Jarafan3@gmail.com>
2026-03-24 22:07:41 +0300

    Add fstream in main

--- afanasev.yaroslav/S2/main.cpp
+++ afanasev.yaroslav/S2/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <fstream>
 #include "stack.hpp"
 #include "queue.hpp"
 
-int main()
+int main(int argc, char * argv[])
 {
+  if (argc > 2)
+  {
+    std::cerr << "too many args\n";
+    return 1;
+  }
+
+  std::ifstream file;
+  std::istream * in = & std::cin;
+
+  if (argc == 2)
+  {
+    file.open(argv[1]);
+
+    if (!file.is_open())
+    {
+      std::cerr << "error open file\n";
+      return 1;
+    }
+    in = & file;
+  }
+
+  afanasev::Stack< long long > res;
+
   return 0;
 }
