eb4eb0204ec2722cccec5d889c7f7ae445bc720c
jar <Jarafan3@gmail.com>
2026-05-28 18:01:34 +0300

    I think about structure of note and make her

--- afanasev.yaroslav/F0/Note.hpp
+++ afanasev.yaroslav/F0/Note.hpp
@@ -1,11 +1,18 @@
 #ifndef NOTE_HPP
 #define NOTE_HPP
 
+#include <HashTable/HashTable.hpp>
+#include <HashTable/HashFunction.hpp>
+#include <top-it-vector.hpp>
+#include <list.hpp>
+
 namespace afanasev
 {
   class Note
   {
-
+    Vector< std::string > lines;
+    HashTable< std::string, bool, Hasher< std::string >, std::equal_to< std::string > > tags;
+    List< std::string > children;
   };
 }
 
