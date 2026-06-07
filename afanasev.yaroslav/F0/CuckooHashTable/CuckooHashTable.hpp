00ea55ff6e7dc939862587091e6c8ee8886376ff
jar <Jarafan3@gmail.com>
2026-06-07 15:32:36 +0300

    Make Intarface Table

--- afanasev.yaroslav/F0/CuckooHashTable/CuckooHashTable.hpp
+++ afanasev.yaroslav/F0/CuckooHashTable/CuckooHashTable.hpp
@@ -5,336 +5,68 @@
 #include <functional>
 #include <stdexcept>
 #include <utility>
-#include <list.hpp>
-#include <top-it-vector.hpp>
 #include "CuckooHashFunction.hpp"
-#include "CuckooHashIter.hpp"
+#include "top-it-vector.hpp"
 
 namespace afanasev
 {
-  template< class Key, class Value, class Hash, class Equal >
-  class HashTable
+  template < class Key, class Value, class Hash1 = Hasher< Key >,
+    class Hash2 = Hasher< Key >, class Equal = std::equal_to< Key > >
+  class CuckooHashTable
   {
-    friend class HashIter< Key, Value, Hash, Equal >;
-    friend class HashConstIter< Key, Value, Hash, Equal >;
+  public:
+    using value_type = std::pair< Key, Value >;
 
-    using type = std::pair< Key, Value >;
+    explicit CuckooHashTable( size_t initial_capacity = 16 );
+    CuckooHashTable( const CuckooHashTable & other );
+    CuckooHashTable( CuckooHashTable && other ) noexcept;
+    ~CuckooHashTable();
 
-    public:
-    using HIter = HashIter< Key, Value, Hash, Equal >;
-    using HCIter = HashConstIter< Key, Value, Hash, Equal >;
+    CuckooHashTable & operator=( const CuckooHashTable & other );
+    CuckooHashTable & operator=( CuckooHashTable && other ) noexcept;
 
-    HIter begin();
-    HIter end();
-    HCIter cbegin() const;
-    HCIter cend() const;
+    void add( const Key & k, const Value & v );        // вставка / обновление
+    void add( Key && k, Value && v );                  // перемещающая версия
 
-    explicit HashTable(size_t slots);
-    HashTable(const HashTable & other);
-    HashTable(HashTable && other) noexcept;
-    HashTable & operator=(const HashTable & other);
-    HashTable & operator=(HashTable && other) noexcept;
-    ~HashTable();
+    Value drop( const Key & k );                       // удалить ключ, вернуть значение
+    bool has( const Key & k ) const noexcept;          // проверить наличие
 
-    void clear() noexcept;
+    Value & get( const Key & k );                      // доступ к значению (неконстантный)
+    const Value & get( const Key & k ) const;          // константный доступ
 
+    void clear() noexcept;                             // очистить таблицу
+    size_t size() const noexcept;                      // количество элементов
+    bool empty() const noexcept;                       // пуста ли таблица
 
-    void add(Key k, Value v);
-    Value drop(Key k);
-    bool has(Key k) const noexcept;
-    void rehash(size_t slots);
-    void swap(HashTable & other) noexcept;
+    void rehash( size_t new_capacity );                // изменить ёмкость (перестроить)
+    void swap( CuckooHashTable & other ) noexcept;     // обмен содержимым
 
-    Value & get(Key k);
-    const Value & get(Key k) const;
+  private:
+    Vector< value_type > data1_;
+    Vector< bool > occupied1_;
+    Vector< value_type > data2_;
+    Vector< bool > occupied2_;
 
-    size_t size() const noexcept;
-    bool empty() const noexcept;
-
-    private:
-    Vector< List< type > > data_;
     size_t capacity_;
     size_t size_;
-    Hash hasher_;
-    Equal comparator_;
+    Hash1 hash1_;
+    Hash2 hash2_;
+    Equal equal_;
+
+    size_t index1( const Key & k ) const { return hash1_( k ) % capacity_; }
+    size_t index2( const Key & k ) const { return hash2_( k ) % capacity_; }
+
+    bool insertInternal( const Key & k, const Value & v );
+    bool insertInternal( Key && k, Value && v );
+
+    Value * findValue( const Key & k ) noexcept;
+    const Value * findValue( const Key & k ) const noexcept;
+
+    bool isOccupied1( size_t idx ) const { return occupied1_[ idx ] == 1; }
+    bool isOccupied2( size_t idx ) const { return occupied2_[ idx ] == 1; }
+    void setOccupied1( size_t idx, bool occ ) { occupied1_[ idx ] = occ ? 1 : 0; }
+    void setOccupied2( size_t idx, bool occ ) { occupied2_[ idx ] = occ ? 1 : 0; }
   };
-}
-
-template < class Key, class Value, class Hash, class Equal >
-typename afanasev::HashTable< Key, Value, Hash, Equal >::HIter
-afanasev::HashTable< Key, Value, Hash, Equal >::begin()
-{
-  return HIter(& data_, capacity_, 0);
-}
-
-template < class Key, class Value, class Hash, class Equal >
-typename afanasev::HashTable< Key, Value, Hash, Equal >::HIter
-afanasev::HashTable< Key, Value, Hash, Equal >::end()
-{
-  return HIter();
-}
-
-template < class Key, class Value, class Hash, class Equal >
-typename afanasev::HashTable< Key, Value, Hash, Equal >::HCIter
-afanasev::HashTable< Key, Value, Hash, Equal >::cbegin() const
-{
-  return HCIter(& data_, capacity_, 0);
-}
-
-template < class Key, class Value, class Hash, class Equal >
-typename afanasev::HashTable< Key, Value, Hash, Equal >::HCIter
-afanasev::HashTable< Key, Value, Hash, Equal >::cend() const
-{
-  return HCIter();
-}
-
-template < class Key, class Value, class Hash, class Equal >
-Value & afanasev::HashTable< Key, Value, Hash, Equal >::get(Key k)
-{
-  size_t idx = hasher_(k) % capacity_;
-  List< type > & bucket = data_[idx];
-  LIter< type > it = bucket.begin();
-  while (it != LIter< type >())
-  {
-    if (comparator_((*it).first, k))
-    {
-      return (*it).second;
-    }
-    ++it;
-  }
-  throw std::out_of_range("Key not found");
-}
-
-template < class Key, class Value, class Hash, class Equal >
-const Value & afanasev::HashTable< Key, Value, Hash, Equal >::get(Key k) const
-{
-  size_t idx = hasher_(k) % capacity_;
-  const List< type > & bucket = data_[idx];
-  LCIter< type > it = bucket.begin();
-  while (it != LCIter< type >())
-  {
-    if (comparator_((*it).first, k))
-    {
-      return (*it).second;
-    }
-    ++it;
-  }
-  throw std::out_of_range("Key not found");
-}
-
-template < class Key, class Value, class Hash, class Equal >
-afanasev::HashTable< Key, Value, Hash, Equal >::HashTable(const HashTable & other):
-  data_(),
-  capacity_(other.capacity_),
-  size_(0),
-  hasher_(other.hasher_),
-  comparator_(other.comparator_)
-{
-  for (size_t i = 0; i < capacity_; ++i)
-  {
-    data_.pushBack(List< type >());
-  }
-
-  for (size_t i = 0; i < capacity_; ++i)
-  {
-    const List< type > & srcBucket = other.data_[i];
-    LCIter< type > it = srcBucket.begin();
-    while (it != LCIter< type >())
-    {
-      data_[i].pushFront(*it);
-      ++size_;
-      ++it;
-    }
-  }
-}
-
-template < class Key, class Value, class Hash, class Equal >
-afanasev::HashTable< Key, Value, Hash, Equal >::HashTable(HashTable && other) noexcept:
-  data_(std::move(other.data_)),
-  capacity_(other.capacity_),
-  size_(other.size_),
-  hasher_(std::move(other.hasher_)),
-  comparator_(std::move(other.comparator_))
-{
-  other.capacity_ = 0;
-  other.size_ = 0;
-  other.data_ = Vector< List < type > >();
-}
-
-template <class Key, class Value, class Hash, class Equal>
-afanasev::HashTable<Key, Value, Hash, Equal> &
-afanasev::HashTable<Key, Value, Hash, Equal>::operator=(const HashTable & other)
-{
-  if (this != & other)
-  {
-    HashTable tmp(other);
-    swap(tmp);
-  }
-  return *this;
-}
-
-template < class Key, class Value, class Hash, class Equal >
-afanasev::HashTable< Key, Value, Hash, Equal > &
-afanasev::HashTable< Key, Value, Hash, Equal >::operator=(HashTable && other) noexcept
-{
-  if (this != & other)
-  {
-    HashTable tmp(std::move(other));
-    swap(tmp);
-  }
-  return *this;
-}
-
-template < class Key, class Value, class Hash, class Equal >
-void afanasev::HashTable< Key, Value, Hash, Equal >::swap(HashTable & other) noexcept
-{
-  data_.swap(other.data_);
-  std::swap(capacity_, other.capacity_);
-  std::swap(size_, other.size_);
-  std::swap(hasher_, other.hasher_);
-  std::swap(comparator_, other.comparator_);
-}
-
-template < class Key, class Value, class Hash, class Equal >
-size_t afanasev::HashTable< Key, Value, Hash, Equal >::size() const noexcept
-{
-  return size_;
-}
-
-template < class Key, class Value, class Hash, class Equal >
-bool afanasev::HashTable< Key, Value, Hash, Equal >::empty() const noexcept
-{
-  return !size_;
-}
-
-template < class Key, class Value, class Hash, class Equal >
-void afanasev::HashTable< Key, Value, Hash, Equal >::rehash(size_t slots)
-{
-  if (slots <= capacity_)
-  {
-    return;
-  }
-
-  Vector< List < type > > new_data;
-
-  for (size_t i = 0; i < slots; ++i)
-  {
-    new_data.pushBack(List< type >());
-  }
-
-  for (size_t i = 0; i < capacity_; ++i)
-  {
-    List< type > & bucket = data_[i];
-    LIter< type > it = bucket.begin();
-
-    while (it != LIter< type >())
-    {
-      size_t new_idx = hasher_((*it).first) % slots;
-      new_data[new_idx].pushFront(*it);
-      ++it;
-    }
-  }
-
-  data_ = std::move(new_data);
-  capacity_ = slots;
-}
-
-template < class Key, class Value, class Hash, class Equal >
-bool afanasev::HashTable< Key, Value, Hash, Equal >::has(Key k) const noexcept
-{
-  size_t idx = hasher_(k) % capacity_;
-  const List< type > & bucket = data_[idx];
-  LCIter< type > it = bucket.begin();
-
-  while (it != LCIter< type >())
-  {
-    if (comparator_((*it).first, k)) return true;
-    ++it;
-  }
-
-  return false;
-}
-
-template < class Key, class Value, class Hash, class Equal >
-Value afanasev::HashTable< Key, Value, Hash, Equal >::drop(Key k)
-{
-  size_t idx = hasher_(k) % capacity_;
-  List< type > & bucket = data_[idx];
-  LIter< type > prev = LIter< type >();
-  LIter< type > it = bucket.begin();
-
-  while (it != LIter< type >())
-  {
-    if (comparator_((*it).first, k))
-    {
-      Value val = (*it).second;
-      if (prev == LIter< type >())
-      {
-        bucket.popFront();
-      }
-      else
-      {
-        bucket.deleteNext(prev);
-      }
-      --size_;
-      return val;
-    }
-    prev = it;
-    ++it;
-  }
-  throw std::out_of_range("Key not found");
-}
-
-template < class Key, class Value, class Hash, class Equal >
-void afanasev::HashTable< Key, Value, Hash, Equal >::add(Key k, Value v)
-{
-  size_t idx = hasher_(k) % capacity_;
-  List< type > & bucket = data_[idx];
-  LIter< type > it = bucket.begin();
-
-  while (it != LIter< type >())
-  {
-    if (comparator_((*it).first, k))
-    {
-      (*it).second = v;
-      return;
-    }
-    ++it;
-  }
-
-  bucket.pushFront(type(k, v));
-  ++size_;
-}
-
-template < class Key, class Value, class Hash, class Equal >
-void afanasev::HashTable< Key, Value, Hash, Equal >::clear() noexcept
-{
-  for (size_t i = 0; i < capacity_; ++i)
-  {
-    data_[i].clear();
-  }
-
-  size_ = 0;
-}
-
-template < class Key, class Value, class Hash, class Equal >
-afanasev::HashTable< Key, Value, Hash, Equal >::HashTable(size_t slots):
-  data_(),
-  capacity_(slots),
-  size_(0),
-  hasher_(Hash{}),
-  comparator_(Equal{})
-{
-  for (size_t i = 0; i < slots; ++i)
-  {
-    data_.pushBack(List< type >());
-  }
-}
-
-template < class Key, class Value, class Hash, class Equal >
-afanasev::HashTable< Key, Value, Hash, Equal >::~HashTable()
-{
-  clear();
 }
 
 #endif
