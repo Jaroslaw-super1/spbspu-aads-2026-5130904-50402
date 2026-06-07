bd940602d554cb2a11aab490461e8bb1aeb0393d
jar <Jarafan3@gmail.com>
2026-06-07 16:07:39 +0300

    Make findValue

--- afanasev.yaroslav/F0/CuckooHashTable/CuckooHashTable.hpp
+++ afanasev.yaroslav/F0/CuckooHashTable/CuckooHashTable.hpp
@@ -391,4 +391,23 @@ insertInternal(Key && k, Value && v)
   return false;
 }
 
+template< class Key, class Value, class Hash1, class Hash2, class Equal >
+Value * afanasev::CuckooHashTable< Key, Value, Hash1, Hash2, Equal >::
+findValue(const Key & k) noexcept
+{
+  size_t i1 = index1(k);
+  if (isOccupied1(i1) && equal_(data1_[i1].first, k))
+  {
+    return &data1_[i1].second;
+  }
+
+  size_t i2 = index2(k);
+  if (isOccupied2(i2) && equal_(data2_[i2].first, k))
+  {
+    return &data2_[i2].second;
+  }
+
+  return nullptr;
+}
+
 #endif
